2a70e1edcf389c4c1ebf5b839994c64be6e76057
Turcu Mihai <mihaiturcu35m@gmail.com>
2023-10-22 13:16:58 +0300

    Tema 2

---
+++ Tema2_IMR/Assets/SphereScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereScript : MonoBehaviour
+{
+    public int puncteDeScor = 10; // Punctele câștigate pentru fiecare lovitură
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            // Obține distanța dintre sferă și țintă
+            float distanta = Vector3.Distance(transform.position, collision.transform.position);
+
+            // Calculează punctele în funcție de distanță (poți ajusta această formulă în funcție de preferințele tale)
+            int puncteCastigate = Mathf.RoundToInt(100f / distanta);
+
+            // Obține script-ul țintei și apelează metoda CresteScor pentru a crește scorul
+            TargetScript targetScript = collision.gameObject.GetComponent<TargetScript>();
+            if (targetScript != null)
+            {
+                targetScript.CresteScor(puncteCastigate);
+            }
+
+            // Dezactivează sfera pentru a o face să dispară după ce lovește ținta
+            gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ Tema2_IMR/Assets/TargetScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; // Asigură-te că ai importat namespace-ul UnityEngine.UI
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    public Text textScore; // Asigură-te că ai asocierile făcute în Editorul Unity
+
+    private int scor = 0;
+
+    public void CresteScor(int puncte)
+    {
+        scor += puncte;
+        textScore.text = "Scor: " + scor; // Actualizează textul scorului pe ecran
+    }
+}
